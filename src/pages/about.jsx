2fa7a05c32bcfcf6efd426daa84f80296c660e1b
JawadAhmed07 <qwertykey41@gmail.com>
2024-09-13 23:00:00 +0500

    3rd check

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,6 +3,7 @@ function About() {
         <>
             <h1>This is About page</h1>
             <h2>About change update check</h2>
+            <h3>3rd time update check</h3>
         </>
     )
 }
