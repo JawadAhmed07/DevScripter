911534f70c322d376f4ea013aa7570fdd3c66044
JawadAhmed07 <qwertykey41@gmail.com>
2024-09-13 22:38:45 +0500

    Repo udate try

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,6 +1,10 @@
 function About (){
     return (
-        <h1>This is About page</h1>
+    <>
+    <h1>This is About page</h1>
+        <h2>about update kia h</h2>
+    
+    </>    
     )
 }
 export default About;
