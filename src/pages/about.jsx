f94674089fa5f601a906f1a0d70a629051269473
Anas raza <154615716+Muhammadanasraza@users.noreply.github.com>
2024-09-15 19:39:40 +0500

    about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,6 +4,7 @@ function About() {
             <h1>This is About page</h1>
             <h2>About change update check</h2>
             <h3>3rd time update check</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A ducimus nulla assumenda debitis dolorem temporibus dicta iure ad incidunt quo eum placeat ut aliquam, mollitia ipsa, necessitatibus culpa labore consequuntur!</p>
         </>
     )
 }
