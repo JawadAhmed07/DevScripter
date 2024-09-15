7d9aa5cbf7a48a05f1acfb3f315f86124782193d
JawadAhmed07 <qwertykey41@gmail.com>
2024-09-15 07:52:05 +0500

    Service page decided

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,6 +1,12 @@
 function Services(){
     return (
+        <>
         <h1>this is service page</h1>
+        <h2>Service page desiced</h2>
+        <h3>drofseo.com</h3>
+        <h3>https://w3torch.com/services/web-development/</h3>
+        
+        </>
     )
 }
 export default Services;
